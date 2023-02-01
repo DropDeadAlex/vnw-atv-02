0290bc25c7d1239450025f6ae4722da64b7d6d86
Alex de Jesus <DropDeadAlex@msn.com>
2023-02-01 10:48:15 -0300

    ex01 closed

--- src/scripts/ex01.js
+++ src/scripts/ex01.js
@@ -1,12 +1,5 @@
-// const log = console.log.bind(console)
-
-// log("está função recebe um valor e exibe como mensagem no console")
-
-
-// export { log }
-
 const consoMessage = function(){
-    console.log("mensagem no console");
+    console.log("mensagem no console")
 }
 
-consoMessage();
+consoMessage()
